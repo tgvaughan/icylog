d0cc08f3942567e068c6f9e088c4424f52ea8cd7
Tim Vaughan <tgvaughan@gmail.com>
2014-08-03 11:44:41 +1200

    Added icytree-esque logos etc.

--- icylog.js
+++ icylog.js
@@ -1,5 +1,9 @@
+var logFile = undefined;
+
 // Page initialisation code:
 $(document).ready(function() {
+
+    $(window).on("resize", update);
 
     // Set up tabs on main panel
     $("#mainPanel").tabs();
@@ -26,7 +30,48 @@ $(document).ready(function() {
         if (!$(this).parent().hasClass("ui-state-disabled"))
             $("#fileInput").trigger("click");
     });
+    $("#fileInput").change(function() {
+        logFile = $("#fileInput").prop("files")[0];
+        loadFile();
+    });
+
+    update();
 });
+
+// Load/reload log file
+function loadFile() {
+}
+
+// Ensure SVG is positioned correctly in drop panel.
+function updateDropPanel() {
+
+    // Update padding on drop panel:
+    var dropPanel = $("#dropPanel");
+    dropPanel.html("");
+    
+    var imgHeight = 136;
+    var imgWidth = 368;
+
+    dropPanel.append(
+        $("<img/>")
+            .attr("src", "icylog_start.svg")
+            .attr("height", imgHeight));
+
+    var pad = Math.max(Math.floor((dropPanel.innerHeight()-imgHeight)/2), 0) + "px";
+    dropPanel.css("paddingTop", pad);
+    dropPanel.css("paddingBottom", pad);
+}
+
+
+// Redraw everything following file (re)load / window size change
+function update() {
+    if (logFile === undefined)
+        updateDropPanel();
+}
+
+/****************************
+          PROTOTYPES
+ ****************************/
 
 // Prototype object representing a log of a single variable
 var VariableLog = Object.create({}, {
